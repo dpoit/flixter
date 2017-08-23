7f15efe6aff7fc187b76ec4ff08fff2cbae007e1
dpoit <dannypoit@gmail.com>
2017-08-22 23:13:24 -0400

    Student enrollment

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,13 +3,13 @@ class EnrollmentsController < ApplicationController
 
   def create
     current_user.enrollments.create(course: current_course)
-    redirect_to course_path(@course)
+    redirect_to course_path(current_course)
   end
 
   private
 
   def current_course
-    @current_course ||= Course.find(params[:id])
+    @current_course ||= Course.find(params[:course_id])
   end
 
   def enrollment_params
