1c5f6b7fb222558aa6b064d2ada3479864db10fc
dpoit <dannypoit@gmail.com>
2019-07-13 21:18:22 -0400

    Added white spaces for readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resource :dashboard, only: [:show]
+
   root 'static_pages#index'
+
   get 'privacy', to: 'static_pages#privacy'
   get 'team', to: 'static_pages#team'
   get 'careers', to: 'static_pages#careers'
+
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
+
   resources :lessons, only: [:show]
+
   namespace :instructor do
     resources :lessons, only: [:update]
+
     resources :sections, only: [:update] do
       resources :lessons, only: [:create]
     end
+    
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
