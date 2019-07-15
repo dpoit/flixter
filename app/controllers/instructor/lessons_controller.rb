34661ec4218e16b56924229ef74d093e9f272f29
dpoit <dannypoit@gmail.com>
2019-07-14 22:05:12 -0400

    Cleaned up line length in instructor lessons controller

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -38,6 +38,7 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
+    params.require(:lesson).permit(:title, :subtitle, :video,
+        :row_order_position)
   end
 end
